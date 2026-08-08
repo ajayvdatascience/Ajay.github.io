c660948b29e31871dba08368a9701b4d93a094c5
ajayvdatascience <ajayv142006@gmail.com>
2026-08-08 17:37:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const menuBtn=document.getElementById('menuBtn');const navMenu=document.getElementById('navMenu');menuBtn.addEventListener('click',()=>navMenu.classList.toggle('open'));document.querySelectorAll('#navMenu a').forEach(a=>a.addEventListener('click',()=>navMenu.classList.remove('open')));
