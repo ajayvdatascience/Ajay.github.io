bad9fb5037723aff8a91a860aa2202d6a9edcc2c
ajayvdatascience <ajayv142006@gmail.com>
2026-08-10 19:50:41 +0530

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-const menuBtn=document.getElementById('menuBtn');const navMenu=document.getElementById('navMenu');menuBtn.addEventListener('click',()=>navMenu.classList.toggle('open'));document.querySelectorAll('#navMenu a').forEach(a=>a.addEventListener('click',()=>navMenu.classList.remove('open')));
